b4fc1a648020b1f98a28e5a52eb37124daf00065
liuwei <86007829@qq.com>
2026-05-31 16:32:21 +0800

    1

--- src/MainClient/AppConsts.cs
+++ src/MainClient/AppConsts.cs
@@ -4,7 +4,7 @@ namespace MainClient
 {
     public class AppConsts
     {
-        public const string AppVersion = "2026.3.0.89";
-        public const string AppPrefix = "SMADNew_";//"SMAD_"
+        public const string AppVersion = "2026.5.0.1";
+        public const string AppPrefix = "ADPC_";//"SMAD_"
     }
 }
